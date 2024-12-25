c0e6638fd93f6923d823fd7c9da900fd842f6328
Joandson Bezerra <36518985+joandson19@users.noreply.github.com>
2024-12-25 20:27:13 -0300

    Update conf.php

--- mapaclientes-googlemaps/config/conf.php
+++ mapaclientes-googlemaps/config/conf.php
@@ -1,16 +1,16 @@
 <?php
 // Defina as coordenadas da localização fixa
-$centralLatitude = "-12.1203";
-$centralLongitude = "-38.4098";
+$centralLatitude = "-12.1203"; // Altere 
+$centralLongitude = "-38.4098"; // Altere
 
 // Defina a sua chave api do google maps
-$googleMapsApiKey = "CHAVEGOOGLEMAPS";
+$googleMapsApiKey = "CHAVEGOOGLEMAPSAPI";
 
 // Token de Proteção da aplicação, ele adiciona uma camada de segurança simples ao mapa! Você pode gerar outro se quiser.
-$validToken = "weecw8pzQMtwZerverwdd32oTGRWpr6gm";
+$validToken = "weecw8pzQMtwZerverwdd32oTGRWpr6gm"; // Altere
 
 // Dados de acesso a api SGP
-$link = "https://LINK_DO_SGP/ws/radius/radacct/list/all/";
+$url = "https://LINKDOSGP";
 $token = "TOKENDOSGP";
 $app = "APP";
 
